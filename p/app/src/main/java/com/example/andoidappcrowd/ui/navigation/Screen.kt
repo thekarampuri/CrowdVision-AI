fff124893690ba3dd6e46a1e064fa0ade4356413
thekarampuri <akhilkarampuri25@gmail.com>
2026-01-22 16:43:25 +0530

    Deleted existing app to re-design

--- p/app/src/main/java/com/example/andoidappcrowd/ui/navigation/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.andoidappcrowd.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Alerts : Screen("alerts")
-    object Map : Screen("map")
-    object CompletedAlerts : Screen("completed_alerts")
-}
